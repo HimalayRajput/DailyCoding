758c333031905228d35e3f6adff7bc42b2e44283
HimalayRajput <himalay.ssm@gmail.com>
2025-01-29 15:13:26 -0500

    Initial commit: Added findK and DailyCoding Java Files

---
+++ DailyCoding.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+
+public class DailyCoding {
+
+    public static void main(String[] args) {
+         //No-Args Constructor
+
+        Scanner input = new Scanner(System.in);
+
+        findK ans =  findK.getUserInput(input);
+
+        ans.displayResult();
+
+    }
+
+   
+}
